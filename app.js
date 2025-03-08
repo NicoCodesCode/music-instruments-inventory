983004b776cbd9296639fe1ab1834815c9b9939e
NicoCodesCode <nicolasrmz08@gmail.com>
2025-03-08 06:41:12 -0600

    add error handling

--- app.js
+++ app.js
@@ -11,5 +11,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res
+    .status(err.statusCode || 500)
+    .render("error", { title: "Error", message: "Internal Server Error :C" });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- controllers/instrumentsController.js
+++ controllers/instrumentsController.js
@@ -1,12 +1,15 @@
 const db = require("../db/queries");
 
-exports.renderAllInstruments = async (req, res) => {
+exports.renderAllInstruments = async (req, res, next) => {
   const categoryQuery = req.query.category;
 
   let instruments = {};
 
-  if (!categoryQuery) instruments = await db.getAllInstruments();
-  else instruments = await db.getInstrumentsByCategory(categoryQuery);
-
-  res.render("index", { title: "Instruments Inventory", instruments });
+  try {
+    if (!categoryQuery) instruments = await db.getAllInstruments();
+    else instruments = await db.getInstrumentsByCategory(categoryQuery);
+    res.render("index", { title: "Instruments Inventory", instruments });
+  } catch (error) {
+    next(error);
+  }
 };
