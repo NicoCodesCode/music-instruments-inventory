6800a4dd3af1b9b18d0a2f3da40a32ada8419119
NicoCodesCode <nicolasrmz08@gmail.com>
2025-03-08 08:50:30 -0600

    add more details

--- db/queries.js
+++ db/queries.js
@@ -16,7 +16,7 @@ exports.getInstrumentsByCategory = async (category) => {
 
 exports.getInstrumentDetailsById = async (instrumentId) => {
   const sqlQuery =
-    "SELECT instruments.instrument_id, brands.brand_name, brands.country, models.model_name, status.status_name, models.specifications, models.year_introduced FROM instruments INNER JOIN models ON instruments.model_id = models.model_id INNER JOIN brands ON models.brand_id = brands.brand_id INNER JOIN status ON instruments.status_id = status.status_id WHERE instrument_id = $1";
+    "SELECT instruments.instrument_id, instruments.price, brands.brand_name, brands.country, models.model_name, models.specifications, models.year_introduced, status.status_name FROM instruments INNER JOIN models ON instruments.model_id = models.model_id INNER JOIN brands ON models.brand_id = brands.brand_id INNER JOIN status ON instruments.status_id = status.status_id WHERE instrument_id = $1";
   const { rows } = await pool.query(sqlQuery, [instrumentId]);
   return rows[0];
 };
